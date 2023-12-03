ff1d843996a4e6fdeefce0256058f0d72b1d54cf
himanshuShukla55 <himanshu.shukla55@gmail.com>
2023-12-04 00:55:39 +0530

    fix : import in search Page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import ListingCard from "../components/listingCard";
+import ListingCard from "../components/ListingCard";
 
 const Search = () => {
   const [searchParams, setSearchParams] = useSearchParams();
