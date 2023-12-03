6622e475467a09b791ad151a9bc49ed965c7c9c3
himanshuShukla55 <himanshu.shukla55@gmail.com>
2023-12-04 00:26:00 +0530

    new : About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">
+        About Real Estate
+      </h1>
+      <p className="mb-4 text-slate-600">
+        Welcome to RealEstate, your premier destination for discovering the
+        perfect property to call home. At RealEstate, we understand that finding
+        the right place to live is a significant decision, and we're here to
+        simplify the process for you. With an extensive and constantly updated
+        list of real estate properties available for rent or sale, our
+        user-friendly platform empowers you to explore a diverse range of
+        options at your fingertips.
+      </p>
+      <p className="mb-4 text-slate-600">
+        Our website features intuitive filters that allow you to tailor your
+        search based on your unique preferences. Whether you're seeking a
+        furnished apartment, an unfurnished house, or a specific type of offer,
+        RealEstate ensures a seamless and personalized browsing experience. From
+        cozy apartments in the heart of the city to spacious homes in tranquil
+        suburbs, we're dedicated to helping you discover a property that suits
+        your lifestyle and meets your every need.
+      </p>
+      <p className="mb-4 text-slate-600">
+        At RealEstate, we prioritize transparency, reliability, and efficiency
+        in connecting potential buyers or renters with property listings. Our
+        commitment is to simplify your real estate journey, making it an
+        enjoyable and stress-free experience. Explore, filter, and find your
+        dream home with RealEstate, where your search for the perfect property
+        begins and ends.
+      </p>
+    </div>
+  );
 };
 
 export default About;
